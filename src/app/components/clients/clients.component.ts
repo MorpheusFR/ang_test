dd11b7a67cf7aef6efa269cf16878e722eb4bd0d
MorpheusFR <Creatorsk@gmail.com>
2018-08-14 02:47:46 +0300

    soft fixing

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,25 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { Client } from '../client';
+import { ClientsService } from "app/components/service/";
+import { Client } from 'src/app/models/client.model';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
+  providers: [ClientsService]
 })
-
 export class ClientsComponent implements OnInit {
- 
-  // clients = CLIENTS;
- 
-  selectedHero: Client;
- 
- 
-  constructor() { }
- 
+  filter: string;
+  clients: Client[] = [];
+  client: Client;
+  constructor( private clientsService: ClientsService) { }
+
   ngOnInit() {
+    this.clientsService.getData()
+    .subscribe(data => {
+      this.clients = data;
+    });
   }
- 
-  onSelect(client: Client): void {
-    this.selectedHero = client;
+  showSelectedClient(client) {
+    this.client = client;
+  }
+  get filteredClients(): Client[] {
+    return this.clients.filter(
+       item => this.filter ? this.searchClientData(item, this.filter.toLowerCase()) : true
+    );
+  }
+
+  searchClientData(client: Client, filter: string): boolean {
+    return Object.keys(client).some(k => {
+      return Object.keys(client[k]).some(l => {
+        return l !== 'avatar' && client[k][l].toLowerCase().includes(filter);
+      });
+    });
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Client } from '../client';
+
+// @Component({
+//   selector: 'app-clients',
+//   templateUrl: './clients.component.html',
+//   styleUrls: ['./clients.component.css']
+// })
+
+// export class ClientsComponent implements OnInit {
+ 
+//   // clients = CLIENTS;
+ 
+//   selectedHero: Client;
+ 
+ 
+//   constructor() { }
+ 
+//   ngOnInit() {
+//   }
+ 
+//   onSelect(client: Client): void {
+//     this.selectedHero = client;
+//   }
+// }
