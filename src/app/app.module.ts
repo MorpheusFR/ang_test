bf8a945c7737a8a5d8b27ec378e9b73084fbef90
MorpheusFR <Creatorsk@gmail.com>
2018-08-13 17:00:43 +0300

    Done The Client Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+// import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clients/clients.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+  client: Client = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
